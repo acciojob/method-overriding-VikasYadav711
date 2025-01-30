9efb111e6febda7db75ebd4ba9053037006102d8
Vikas Yadav <128998971+VikasYadav711@users.noreply.github.com>
2025-01-31 00:47:13 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,31 @@
+// package com.driver.test;
+
+// import static org.junit.Assert.*;
+// import org.junit.jupiter.api.Test;
+
+// public class TestCases {
+    
+// }
+
 package com.driver.test;
+
+import com.driver.A;  // Import class A
+import com.driver.B;  // Import class B
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+
+    @Test
+    public void testMethod() {
+        // Create object of B (which inherits from A)
+        B obj = new B();
+
+        // Call overridden method
+        String result = obj.meth();
+
+        // Assert that the overridden method works
+        assertEquals("Method is overridden in Extended class B", result);
+    }
 }
