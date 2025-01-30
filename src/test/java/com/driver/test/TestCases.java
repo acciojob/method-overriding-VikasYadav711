699a95946b61a58709f563baa9c0211178880081
Vikas Yadav <128998971+VikasYadav711@users.noreply.github.com>
2025-01-31 00:27:59 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,30 @@
+// package com.driver.test;
+
+// import static org.junit.Assert.*;
+// import org.junit.jupiter.api.Test;
+
+// public class TestCases {
+    
+// }
+
 package com.driver.test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import com.driver.Main.A;
+import com.driver.Main.B;
 
 public class TestCases {
     
+    @Test
+    public void testMethodA() {
+        A a = new A();
+        assertEquals("Invoking method from class A", a.meth());
+    }
+
+    @Test
+    public void testMethodB() {
+        B b = new B();
+        assertEquals("Method is overridden in Extended class B", b.meth());
+    }
 }
